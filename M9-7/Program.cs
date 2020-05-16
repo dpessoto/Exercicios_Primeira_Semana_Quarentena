b3ed081843ff7aa3351447d6e0bb45f6c80d34b3
Daniel Pessoto <d.pessoto@hotmail.com>
2020-05-16 17:05:37 -0300

    M9-7

---
+++ M9-7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace M9_7 {
+
+    class Classe1 : Classe2 {
+
+        public Classe1() {
+            Console.WriteLine("Lugar1");
+        }
+
+    }
+
+    class Classe2{
+
+        public Classe2() {
+            Console.WriteLine("Lugar2");
+        }
+
+    }
+
+    class Program {
+        static void Main(string[] args) {
+
+            Classe1 c1 = new Classe1();
+            Classe2 c2 = new Classe2();
+        }
+    }
+}
