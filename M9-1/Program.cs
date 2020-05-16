02edcf853e1f121d0682a4a5423f82e27054e96d
Daniel Pessoto <d.pessoto@hotmail.com>
2020-05-16 15:38:33 -0300

    M9 - 1

---
+++ M9-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M9_1 {
+
+    class Aluno {
+        public String nome;
+    }
+
+    class Program {
+        static void Main(string[] args) {
+
+            new Aluno();
+            new Aluno();
+
+        }
+    }
+}
