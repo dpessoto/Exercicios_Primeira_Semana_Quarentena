ceda6ed4fe294714fd8f7e9f1faa9144b13891b9
Daniel Pessoto <d.pessoto@hotmail.com>
2020-05-16 16:10:53 -0300

    M9 - 2

---
+++ M9-2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace M9_2 {
+
+    public class Classe1 {
+        double a1, a2, a3;
+
+        public Classe1(double p) : this(p,p) {
+            Console.WriteLine("construtor com 1 parametro: ");
+        }
+
+        public Classe1(double p12, double p3) : this(p12, p12, p3) {
+            Console.WriteLine("construtor com 2 parametros");
+        }
+
+        public Classe1(double p1, double p2, double p3)  {
+            Console.WriteLine("construtor com 3 parametros: ");
+        }
+
+    }
+
+    class Program {
+        static void Main(string[] args) {
+
+            Classe1 c1 = new Classe1(1, 1);
+        }
+    }
+}
