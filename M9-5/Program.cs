c77a6699951601bf1bde63bead6a4f48d12e9c83
Daniel Pessoto <d.pessoto@hotmail.com>
2020-05-16 16:43:55 -0300

    M9 - 5

---
+++ M9-5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace M9_5 {
+
+    public class Classe1 {
+
+        private string nome;
+        private float total = 0;
+
+        public void setNome(string nome) {
+            this.nome = nome;
+        }
+        public string getNome() {
+            return this.nome;
+        }
+
+        public float getTotal() {
+            return this.total;
+        }
+
+        public void incrementa(float valor) {
+            this.total += valor;
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+
+            Classe1 c1 = new Classe1();
+            c1.setNome("Galileu");
+            c1.incrementa(100);
+            Console.WriteLine("{0} {1}\n", c1.getNome(), c1.getTotal());
+
+        }
+    }
+}
