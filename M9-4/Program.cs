a07032a3d9b1e0645bd59afc7e6845fa959e8037
Daniel Pessoto <d.pessoto@hotmail.com>
2020-05-16 16:32:55 -0300

    M9 - 4

---
+++ M9-4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace M9_4 {
+
+    public class Classe1 {
+
+        public float d1;
+
+        public Classe1(float p) {
+            this.d1 = p;
+        }
+
+        public void metodo1(float m, float n) {
+            d1 *= (m + n);
+        }
+
+        public void metodo1(float m) {
+            this.metodo1(m, m);
+            d1 *= m ;
+        }
+
+        public void metodo1() {
+            this.metodo1(this.d1);
+        }
+
+    }
+
+
+    class Program {
+        static void Main(string[] args) {
+
+            Classe1 c1 = new Classe1(3);
+            c1.metodo1();
+            Console.WriteLine(c1.d1);
+
+        }
+    }
+}
